bf35d47cb5b72ac04ed49a4755e2bf2b2308b1a7
Charis Williamson <cpt533@york.ac.uk>
2026-03-13 20:37:00 +0000

    read tasks from file

--- todo.py
+++ todo.py
@@ -1,38 +1,31 @@
 import argparse
-import os
 
 TASK_FILE = ".tasks.txt"
 
+
 def add_task(task):
     """Function: add_task
-    
+
     Input - a task to add to the list
     Return - nothing
     """
 
+
 def list_tasks():
-    return
+    with open(TASK_FILE, "r") as file:
+        tasks = file.readlines()
+    return tasks
 
 
 def remove_task(index):
     return
 
+
 def main():
     parser = argparse.ArgumentParser(description="Command-line Todo List")
-    parser.add_argument(
-            "-a",
-            "--add",
-            help="Add a new task"
-            )
-    parser.add_argument(
-            "-l",
-            "--list",
-            action="store_true",
-            help="List all tasks")
-    parser.add_argument(
-            "-r",
-            "--remove",
-            help="Remove a task by index")
+    parser.add_argument("-a", "--add", help="Add a new task")
+    parser.add_argument("-l", "--list", action="store_true", help="List all tasks")
+    parser.add_argument("-r", "--remove", help="Remove a task by index")
 
     args = parser.parse_args()
 
